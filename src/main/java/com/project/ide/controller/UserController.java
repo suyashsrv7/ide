9d71a5f106437961ad5c9a6e2faf20dc8e39659f
Suyash Srivastava <suyashsrv7@gmail.com>
2020-05-19 17:09:16 +0530

    Added UserController for test

---
+++ src/main/java/com/project/ide/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.project.ide.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ResponseEntity<?> hello() {
+		return ResponseEntity.ok("Hello world");
+	}
+}
+	
